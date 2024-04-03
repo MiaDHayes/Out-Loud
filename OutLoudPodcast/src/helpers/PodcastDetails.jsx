0eab0af025b9d1add19e4ee4488f908c0c294177
MiaDHayes <miadeyon@outlook.com>
2024-04-03 15:51:39 -0500

    podcastform and podcastdetails are almost completed

--- OutLoudPodcast/src/helpers/PodcastDetails.jsx
+++ OutLoudPodcast/src/helpers/PodcastDetails.jsx
@@ -1,14 +1,44 @@
+import { useState } from "react"
+import axios from 'axios'
 
 
-function PodcastDetail() {
+function PodcastDetails({ onSubmit }) {
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        try {
+            const response = await axios.post('http://localhost:3005/podcast', {
+                title, description
+            })
+
+            setTitle('')
+            setDescription('')
+
+            console.log('Response from server:', error)
+            onSubmit()
+        } catch (error) {
+            console.error('Error submitting form:', error)
+        }
+    }
+
 
     return (
-        <div className= "detail-container">
-            <h2>{title}</h2>
-            <p>{description}</p>
-        </div>
+        <form onSubmit={handleSubmit} className="detail-container">
+            <div>
+                <label htmlFor= "title">Title:</label>
+                <input type= "text" value={title} onChange={(e) => setTitle(e.target.value)}
+                required
+                />
+                <label htmlFor="description">Description:</label>
+                <textarea value={description} onChange={(e) => setDescription(e.target.value)}
+                required></textarea>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
     )
 }
 
 
-export default PodcastDetail
+export default PodcastDetails
