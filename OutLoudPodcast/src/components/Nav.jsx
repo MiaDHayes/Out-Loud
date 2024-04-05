0f2780c075eea63d74268dca1aedd249dfadb856
MiaDHayes <miadeyon@outlook.com>
2024-04-05 14:03:29 -0500

    styling

--- OutLoudPodcast/src/components/Nav.jsx
+++ OutLoudPodcast/src/components/Nav.jsx
@@ -5,6 +5,7 @@ function Nav() {
         <nav className= 'navbar'>
             <div className= 'topLevelLink'>
                 <Link className= 'createLink' to= '/create-podcast'>Create Podcast</Link>
+                <Link className= 'allLink' to= '/all-podcasts'>All Podcasts</Link>
             </div>
         </nav>
     )
--- OutLoudPodcast/src/components/PodcastList.jsx
+++ OutLoudPodcast/src/components/PodcastList.jsx
@@ -18,23 +18,27 @@ function PodcastList() {
     <div>
       <h2>Podcasts</h2>
       <ul>
-        {podcasts.map(podcast => (
-          <li key={podcast._id}>
-            <img
-              src={`/uploads/coverPhotos/${podcast.coverPhoto}`}
-              alt='Cover'
-              onLoad={() => console.log('Image loaded:', `/coverPhoto/${podcast.coverPhoto}`)}
-              onError={() => console.error('Image not loaded:', `/coverPhoto/${podcast.coverPhoto}`)}
-            />
-            <h3>{podcast.title}</h3>
-            <p>{podcast.description}</p>
-  //The source needs to be changed to uploads/podcastFile
-            <audio controls>
-              <source src={podcast.audioClip} type='audio/mpeg' />
-              Your browser does not support the audio element
-            </audio>
-          </li>
-        ))}
+        {podcasts.map(podcast => {
+          console.log('Podcast file:', podcast.podcastFile)
+            return (
+            <li key={podcast._id}>
+              <img
+                src={`/uploads/coverPhotos/${podcast.coverPhoto}`}
+                alt='Cover'
+                onLoad={() => console.log('Image loaded:', `/coverPhoto/${podcast.coverPhoto}`)}
+                onError={() => console.error('Image not loaded:', `/coverPhoto/${podcast.coverPhoto}`)}
+              />
+              <h3>{podcast.title}</h3>
+              <p>{podcast.description}</p>
+    {/* //The source needs to be changed to uploads/podcastFile */}
+              <audio controls>
+                <source src={`/uploads/podcastFiles/${podcast.podcastFile}`} type='audio/mpeg' />
+                Your browser does not support the audio element
+                
+              </audio>
+            </li>
+          )
+          })}
       </ul>
     </div>
   )
