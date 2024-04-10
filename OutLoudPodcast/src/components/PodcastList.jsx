f45356f9491e0816825ee7e8238f3bd40bdc5536
MiaDHayes <miadeyon@outlook.com>
2024-04-10 17:38:38 -0500

    swiper component

--- OutLoudPodcast/src/components/PodcastList.jsx
+++ OutLoudPodcast/src/components/PodcastList.jsx
@@ -1,15 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import ReactPlayer from 'react-player'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
-import Nav from './Nav'
+import PodcastSwiper from '../helpers/PodcastSwiper'
+
 
 
 function PodcastList() {
   const [podcasts, setPodcasts] = useState([])
   const [error, setError] = useState(null)
   const navigate = useNavigate()
+  const featuredPodcasts = podcasts.slice(0, 3)
 
   useEffect(() => {
     
@@ -29,8 +30,8 @@ function PodcastList() {
 
   return (
     <div className= 'bridgerton'>
-      <Nav />
-      <h2>Podcasts</h2>
+      <Link to= '/all-podcasts' className='allLinks'>All Podcasts</Link>
+      <h2>Browse</h2>
       <ul>
         {podcasts.map(podcast => {
           console.log('Podcast file:', podcast.podcastFile)
@@ -46,22 +47,85 @@ function PodcastList() {
                 <h6>{podcast.username}</h6>
                 <h3>{podcast.title}</h3>
                 <p>{podcast.description}</p>
-                {/* <ReactPlayer 
-                  url={`/uploads/podcastFiles/${podcast.podcastFile}`}
-                  controls= {true}
-                  playing= {true}
-                  width= "100%"
-                  height= "50px"
-                  onError={(error) => console.error('Error playing podcast:', error)}
-                /> */}
               </Link>  
             </li>
           )
           })}
       </ul>
+      <PodcastSwiper podcasts={featuredPodcasts} title= 'Featured Podcasts'/>
       <button id='back' onClick={() => navigate('/home')}>Go Back</button>
     </div>
   )
 }
 
 export default PodcastList
+
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import { Link } from 'react-router-dom';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import 'swiper/css';
+
+
+// function PodcastList() {
+//   const [podcasts, setPodcasts] = useState([]);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     const fetchPodcasts = async () => {
+//       try {
+//         const response = await axios.get('http://localhost:3005/podcasts');
+//         setPodcasts(response.data);
+//       } catch (error) {
+//         setError(error);
+//         console.error('Error fetching podcasts:', error);
+//       }
+//     };
+
+//     fetchPodcasts();
+//   }, []);
+
+//   if (error) {
+//     return <div>Error fetching podcasts: {error.message}</div>;
+//   }
+
+//   return (
+//     <div className="podcast-feed">
+//       <Link to="/all-podcasts" className="all-links">
+//         All Podcasts
+//       </Link>
+//       <h2>Browse</h2>
+
+//       {podcasts.length > 0 ? (
+//         <Swiper
+//           spaceBetween={2} // Adjust spacing between slides
+//           slidesPerView={"auto"} // Adjust number of slides based on screen size
+//           navigation={true} // Enable navigation arrows
+//         >
+//           {podcasts.map((podcast) => (
+//             <SwiperSlide key={podcast._id}>
+//               <Link to={`/podcast/${podcast._id}`}>
+//                 <img
+//                   src={`http://localhost:3005/${podcast.coverPhoto}`}
+//                   alt={podcast.title}
+//                   onError={(e) => console.error('Image not loaded:', e.target.src)}
+//                 />
+//                 <h6>{podcast.username}</h6>
+//                 <h3>{podcast.title}</h3>
+//                 <p>{podcast.description}</p>
+//               </Link>
+//             </SwiperSlide>
+//           ))}
+//         </Swiper>
+//       ) : (
+//         <p>No podcasts found.</p>
+//       )}
+
+//       <button id="back" onClick={() => navigate('/home')}>
+//         Go Back
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default PodcastList;
